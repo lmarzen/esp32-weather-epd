ffa641fb48a196547c009d3699467f8dc3becdfa
Luke Marzen <marzenlj@gmail.com>
2022-03-08 10:55:48 -0600

    created arduino sketch

---
+++ esp32_weather_epd/esp32_weather_epd.ino
@@ -0,0 +1,19 @@
+#include "config.h"
+#include <ArduinoJson.h>              // https://github.com/bblanchon/ArduinoJson
+#include <WiFi.h>
+#include "time.h"
+#include <SPI.h>
+#define  ENABLE_GxEPD2_display 1
+#include <GxEPD2_BW.h>
+//#include <GxEPD2_3C.h>
+#include <U8g2_for_Adafruit_GFX.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
