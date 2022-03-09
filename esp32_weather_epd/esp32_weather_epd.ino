21cdb4a359a5847121d55ee42c28a33ece135c76
Luke Marzen <marzenlj@gmail.com>
2022-03-09 02:29:47 -0600

    Added wifi and time setup and sleep

--- esp32_weather_epd/esp32_weather_epd.ino
+++ esp32_weather_epd/esp32_weather_epd.ino
@@ -1,19 +1,187 @@
-#include "config.h"
-#include <ArduinoJson.h>              // https://github.com/bblanchon/ArduinoJson
+// built-in libraries
+#include <SPI.h>
 #include <WiFi.h>
 #include "time.h"
-#include <SPI.h>
-#define  ENABLE_GxEPD2_display 1
+
+// additional libraries
+#include <ArduinoJson.h>
 #include <GxEPD2_BW.h>
-//#include <GxEPD2_3C.h>
 #include <U8g2_for_Adafruit_GFX.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// header files
+#include "config.h"
+#include "lang.h"
 
+// GLOBAL VARIABLES
+tm timeinfo;
+int     wifi_signal;
+long    start_time = 0;
+String  time_str, date_str;
+
+void setup() {
+  setCpuFrequencyMhz(80); // change cpu freq from 240MHz to 80MHz to conserve battery. much of our time will be waiting to receive data or for screen to update.
+  start_time = millis();
+  Serial.begin(115200);
+
+  // TODO will firebeetle led stay off when on battery? otheriwse desolder...
+//#ifdef LED_BUILTIN
+//  pinMode(LED_BUILTIN, INPUT); // If it's On, turn it off and some boards use GPIO-5 for SPI-SS, which remains low after screen use
+//  digitalWrite(LED_BUILTIN, HIGH);
+//#endif
+
+  if (StartWiFi() == WL_CONNECTED && SetupTime() == true) {
+    UpdateTimeDateStrings();
+    /*
+    if ((CurrentHour >= WakeupTime && CurrentHour <= SleepTime) || DebugDisplayUpdate) {
+      //InitialiseDisplay(); // Give screen time to initialise by getting weather data!
+      uint8_t Attempts = 1;
+      bool RxWeather = false, RxForecast = false;
+      WiFiClient client;   // wifi client object
+      while ((RxWeather == false || RxForecast == false) && Attempts <= 2) { // Try up-to 2 time for Weather and Forecast data
+        if (RxWeather  == false) RxWeather  = obtain_wx_data(client, "weather");
+        if (RxForecast == false) RxForecast = obtain_wx_data(client, "forecast");
+        Attempts++;
+      }
+      if (RxWeather && RxForecast) { // Only if received both Weather or Forecast proceed
+        StopWiFi(); // Reduces power consumption
+        DisplayWeather();
+        display.display(false); // Full screen update mode
+      }
+    }
+    */
+  }
+  BeginSleep();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+//
+//
+//    //connect to WiFi
+//  Serial.printf("Connecting to %s ", ssid);
+//  WiFi.begin(ssid, password);
+//  while (WiFi.status() != WL_CONNECTED) {
+//      delay(500);
+//      Serial.print(".");
+//  }
+//  Serial.println(" CONNECTED");
+//  
+//  //init and get the time
+//  configTime(0, 0, ntpServer1); // We will pass 0 for gmtOffset and daylightOffset and use setenv() for timezone offsets
+//  setenv("TZ", timezone, 1);
+//  printLocalTime();
+//
+//Serial.print("ip: ");
+//  Serial.println(WiFi.localIP());
+//  
+//  // disconnect WiFi as it's no longer needed
+//  WiFi.disconnect();
+//  WiFi.mode(WIFI_OFF);
+//
+//  // TODO
+//  // Make OWM API call
+//  // Update Display
+//
+//  BeginSleep();
+//}
 
+void loop() {
+  // this will never run
+}
+
+
+void printLocalTime()
+{
+  if(!getLocalTime(&timeinfo, 10000)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+
+uint8_t StartWiFi() {
+  Serial.printf("Connecting to '%s'", ssid);
+  IPAddress dns(8, 8, 8, 8); // Google DNS
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA); // switch off AP
+  WiFi.setAutoConnect(true);
+  WiFi.setAutoReconnect(true);
+  WiFi.begin(ssid, password);
+  unsigned long start = millis();
+  uint8_t connectionStatus;
+  bool AttemptConnection = true;
+  while (AttemptConnection) {
+    connectionStatus = WiFi.status();
+    if (millis() > start + 15000) { // Wait 15-secs before timing out
+      AttemptConnection = false;
+    }
+    if (connectionStatus == WL_CONNECTED || connectionStatus == WL_CONNECT_FAILED) {
+      AttemptConnection = false;
+    }
+    delay(50);
+    Serial.print(".");
+  }
+  Serial.println();
+  if (connectionStatus == WL_CONNECTED) {
+    wifi_signal = WiFi.RSSI(); // get Wifi signal strength now, because the WiFi will be turned off to save power!
+    Serial.println("IP: " + WiFi.localIP().toString());
+  } else {
+    Serial.printf("Could not connect to %s\n", ssid);
+  }
+  return connectionStatus;
+}
+
+void StopWiFi() {
+  WiFi.disconnect();
+  WiFi.mode(WIFI_OFF);
+}
+
+boolean SetupTime() {
+  configTime(0, 0, ntpServer1, ntpServer2); // We will pass 0 for gmtOffset_sec and daylightOffset_sec and use setenv() for timezone offsets
+  setenv("TZ", timezone, 1);
+  tzset();
+  if (!getLocalTime(&timeinfo, 10000)) {
+    Serial.println("Failed to obtain time");
+    return false;
+  }
+  Serial.println(&timeinfo, "Time setup complete at: %A, %B %d %Y %H:%M:%S");
+  return true;
+}
+
+// TODO
+boolean UpdateTimeDateStrings() {
+  char time_output[30], day_output[30], update_time[30];
+  // see http://www.cplusplus.com/reference/ctime/strftime/
+  if (units[0] == 'm') {
+    if ((!strcmp(lang,"cz")) || !(strcmp(lang,"de")) || !(strcmp(lang,"pl")) || !(strcmp(lang,"nl"))){
+      sprintf(day_output, "%s, %02u. %s %04u", weekday_D[timeinfo.tm_wday], timeinfo.tm_mday, month_M[timeinfo.tm_mon], (timeinfo.tm_year) + 1900); // day_output >> So., 23. Juni 2019 <<
+    }
+    else
+    {
+      sprintf(day_output, "%s %02u-%s-%04u", weekday_D[timeinfo.tm_wday], timeinfo.tm_mday, month_M[timeinfo.tm_mon], (timeinfo.tm_year) + 1900);
+    }
+    strftime(update_time, sizeof(update_time), "%H:%M:%S", &timeinfo);  // Creates: '14:05:49'
+    sprintf(time_output, "%s %s", TXT_UPDATED, update_time);
+  }
+  else
+  {
+    strftime(day_output, sizeof(day_output), "%a %b-%d-%Y", &timeinfo); // Creates  'Sat May-31-2019'
+    strftime(update_time, sizeof(update_time), "%r", &timeinfo);        // Creates: '02:05:49pm'
+    sprintf(time_output, "%s %s", TXT_UPDATED, update_time);
+  }
+  date_str = day_output;
+  time_str = time_output;
+  return true;
+}
+
+void BeginSleep() {
+  // TODO
+  //display.powerOff();
+  if (!getLocalTime(&timeinfo, 10000)) {
+    Serial.println("Failed to obtain time before deep-sleep, using old time.");
+  }
+  long sleep_timer = (sleep_dur * 60 - ((timeinfo.tm_min % sleep_dur) * 60 + timeinfo.tm_sec));
+  esp_sleep_enable_timer_wakeup((sleep_timer + 1) * 1000000LL); // Add 1s extra sleep to allow for fast ESP32 RTC timers
+  Serial.println("Awake for: " + String((millis() - start_time) / 1000.0, 3) + "s");
+  Serial.println("Entering deep-sleep for " + String(sleep_timer) + "(+1)s");
+  delay(250);
+  esp_deep_sleep_start();
 }
